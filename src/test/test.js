a62a36da24a1351f7e3b4ffb35125d169eafc2a9
christian-bunse <christian.bunse@actano.de>
2016-07-12 12:15:09 +0200

    Initial Setup

---
+++ src/test/test.js
@@ -0,0 +1,10 @@
+const {expect} = require('chai')
+const {loadDiseases} = require('../layer_1')
+
+describe('first test', () => {
+    it('should load diseases', () => {
+        result = loadDiseases()
+        console.log(result)
+        expect(result).to.be.a.array
+    })    
+})
