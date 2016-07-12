d98554b741aadbfd6249a8da40022b4f1120c712
Thomas Kölling <spamdoggi@gmail.com>
2016-07-12 14:30:22 +0200

    Add flow and babel to testworkflow, remove build folder

--- build/layer_2/index.js
+++
@@ -1,6 +0,0 @@
-const {
-    MedicineOtherLayer: Medicine,
-    BeerOtherLayer: Beer,
-    loadDiseasesOtherLayer: loadDiseases,
-    loadMedicinsOtherLayer: loadMedicins
-} = require('../layer_1');
